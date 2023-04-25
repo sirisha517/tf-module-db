e102acb497df06450781cd401254e41ec782b7b7
sirisha517 <sai.sirifriends8790@gmail.com>
2023-04-25 16:37:35 +0530

    open application port

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ resource "aws_docdb_cluster" "main" {
   db_subnet_group_name    = aws_docdb_subnet_group.main.name
   kms_key_id              = data.aws_kms_key.key.arn
   storage_encrypted       = var.storage_encrypted
+  vpc_security_group_ids  = [aws_security_group.main.id]
 }
-
 resource "aws_docdb_cluster_instance" "cluster_instances" {
   count              = var.no_of_instances
   identifier         = "${var.env}-docdb-${count.index}"
@@ -29,6 +29,34 @@ resource "aws_docdb_subnet_group" "main" {
     { Name = "${var.env}-subnet_ids" }
   )
 }
+
+resource "aws_security_group" "main" {
+  name        = "docdb-${var.env}"
+  description = "docdb-${var.env}"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "DOCDB"
+    from_port   = 27017
+    to_port     = 27017
+    protocol    = "tcp"
+    cidr_blocks = var.allow_subnets
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = merge(
+    var.tags,
+    { Name = "docdb-${var.env}" }
+  )
+}
+
 resource "aws_ssm_parameter" "docdb_url_catalogue" {
   name  = "${var.env}.docdb.url.catalogue"
   type  = "String"
